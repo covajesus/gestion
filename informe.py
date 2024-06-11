1d83e8167db3a51503fcb562861973ec1b51f567
Rodrigo Cabezas <88550981+rorocabezas@users.noreply.github.com>
2024-06-11 17:20:06 -0400

    Update informe.py

--- informe.py
+++ informe.py
@@ -244,7 +244,8 @@ def main(authenticated=False):
         desv_formatted = sum_total_row['desv']
         ticket_promedio_formatted = format_currency(sum_total_row['ticket_prom_act'])
         #num_sucursales = df_filtrado['sucursal'].nunique()
-	num_sucursales = df_filtrado[df_filtrado['Ingresos_Act'] > 0][df_filtrado['sucursal'] != 'Total']['sucursal'].nunique()    
+	#num_sucursales = df_filtrado[df_filtrado['Ingresos_Act'] > 0][df_filtrado['sucursal'] != 'Total']['sucursal'].nunique()  
+	num_sucursales = df_filtrado[df_filtrado['Ingresos_Act'] > 100][df_filtrado['sucursal'] != 'Total']['sucursal'].nunique()
         flujo = format_valor(sum_total_row['ticket_number'])
         
         # Obtener Calculos para Permanencia
